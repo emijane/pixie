e9d598f6192d86a198ed208bea02685150638c13
Emma <88465944+emijane@users.noreply.github.com>
2023-07-10 11:13:51 -0400

    added mobile menu

--- pixie-app/tailwind.config.js
+++ pixie-app/tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
   ],
   theme: {
     colors: {
-      purple: '#8E6DFF',
+      purple: {
+        xl: '#f1edff',
+        l: '#d2c4ff',
+        default:'#8E6DFF',
+        dark:'#7952ff',
+      },
       transparent: 'transparent',
       current: 'currentColor',
       black: colors.black,
@@ -17,6 +22,22 @@ module.exports = {
       emerald: colors.emerald,
       indigo: colors.indigo,
       yellow: colors.yellow,
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+    
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+    
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+    
+      'xl': '1300px',
+      // => @media (min-width: 1200px) { ... }
+
+      'xxl': '1500px',
+      // => @media (min-width: 1200px) { ... }
     },
     extend: {},
   },
