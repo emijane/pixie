6a1d837ed30cc23704f5005f03ff940146f055df
Emma <88465944+emijane@users.noreply.github.com>
2023-07-11 13:38:42 -0400

    added card components

--- pixie-app/src/App.js
+++ pixie-app/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Stylesheet from './pages/Stylesheet';
 import Home from './pages/Home';
+import Mediapage from './pages/Mediapage';
 
 function App() {
   return (
       <Routes>
         <Route index element={<Home />} />
         <Route path='stylesheet' element={<Stylesheet />} />
+        <Route path='mediapage' element={<Mediapage />} />
       </Routes>
   );
 }
